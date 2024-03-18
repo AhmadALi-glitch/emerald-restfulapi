afee84537ff428482ba5a54a826fa01ed47ddb34
Ahmad Ali <newTownElectroo@gmail.com>
2024-03-18 04:57:43 +0300

    add Team Model

--- api/controllers/v1/account.ts
+++ api/controllers/v1/account.ts
@@ -1,10 +1,21 @@
 
 import express from "express";
+import { prisma } from "../../../database/adapter";
 
 const router = express.Router();
 
-router.get("/:id", (req, res) => {
-    res.send(req.params.id).status(200);
+router.get("/", async (req, res) => {
+    try {
+        await prisma.$connect()
+
+        const result = await prisma.account.findMany()
+        res.send(result)
+
+        await prisma.$disconnect()
+    } catch(exp) {
+        await prisma.$disconnect()
+        res.send(exp).status(500)
+    }
 })
 
 router.post('/create', (req, res) => {
